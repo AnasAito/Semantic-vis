1b163173790945b0a7b595b2d9d885973caf8bd4
Anas AIT AOMAR <56308112+AnasAito@users.noreply.github.com>
2020-12-23 14:29:01 +0100

    add handleclick to nodes

--- src/App.js
+++ src/App.js
@@ -4,33 +4,6 @@ import "./styles.css";
 import ExpandableGraph from "./comps/expandable";
 // Random tree
 
-const gData = {
-  nodes: [
-    { id: "Learning Task Knowledge from Dialog and Web Access" },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 }
-  ],
-  links: [
-    {
-      source: 3,
-      target: 2
-    },
-    {
-      source: "Learning Task Knowledge from Dialog and Web Access",
-      target: 4
-    },
-    {
-      source: "Learning Task Knowledge from Dialog and Web Access",
-      target: 5
-    },
-    {
-      source: 3,
-      target: 4
-    }
-  ]
-};
 const genRandomTree = (N = 300, reverse = false) => {
   return {
     nodes: [...Array(N).keys()].map((i) => ({ id: i })),
@@ -43,10 +16,9 @@ const genRandomTree = (N = 300, reverse = false) => {
   };
 };
 export default function App() {
-  console.log(gData);
   return (
     <div className="App">
-      <ExpandableGraph graphData={genRandomTree(20, true)} />
+      <ExpandableGraph graphData={genRandomTree(4, true)} />
     </div>
   );
 }
