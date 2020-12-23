9306c86091a3a0271d534605c1297b89cb86f250
AnasAito <anasaitaomar1999@gmail.com>
2020-12-24 00:34:52 +0100

    feat : change view button

--- src/comps/expandable.js
+++ src/comps/expandable.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
-import { ForceGraph3D } from "react-force-graph";
+import { ForceGraph2D, ForceGraph3D } from "react-force-graph";
 import { get_shemantic_paper, process_shemantic_data } from "../api";
 export default function ExpandableGraph({ graphData, root_id }) {
   const [prunedTree, setPrunedTree] = useState(graphData);
-
+  const [view, setView] = useState("2d");
   const handleNodeClick = async (node) => {
     //  node.collapsed = !node.collapsed; // toggle collapse state
     const data = await get_shemantic_paper(node.id);
     console.log("got data");
     //console.log(data);
-    const nodes = process_shemantic_data(data, 2);
+    const nodes = process_shemantic_data(data, 10);
     // todo  fix duplicate
     nodes.map((node_) => {
       const newLink =
@@ -19,21 +19,38 @@ export default function ExpandableGraph({ graphData, root_id }) {
       setPrunedTree(({ nodes, links }) => {
         return {
           nodes: [...nodes, { id: node_.id }],
-          links: [...links, newLink]
+          links: [...links, newLink],
         };
       });
     });
   };
   console.log(prunedTree);
   return (
-    <ForceGraph3D
-      graphData={prunedTree}
-      linkDirectionalArrowLength={3.5}
-      linkDirectionalArrowRelPos={1}
-      linkCurvature={0.25}
-      //enableNodeDrag={false}
-      linkDirectionalParticles={2}
-      onNodeClick={handleNodeClick}
-    />
+    <div>
+      <button onClick={() => setView(view === "2d" ? "3d" : "2d")}>
+        change view
+      </button>
+      {view === "2d" ? (
+        <ForceGraph2D
+          graphData={prunedTree}
+          linkDirectionalArrowLength={3.5}
+          linkDirectionalArrowRelPos={1}
+          linkCurvature={0.25}
+          //enableNodeDrag={false}
+          linkDirectionalParticles={2}
+          onNodeClick={handleNodeClick}
+        />
+      ) : (
+        <ForceGraph3D
+          graphData={prunedTree}
+          linkDirectionalArrowLength={3.5}
+          linkDirectionalArrowRelPos={1}
+          linkCurvature={0.25}
+          //enableNodeDrag={false}
+          linkDirectionalParticles={2}
+          onNodeClick={handleNodeClick}
+        />
+      )}
+    </div>
   );
 }
